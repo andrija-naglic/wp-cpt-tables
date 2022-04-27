d3b3f0b82878fdbb336d7f820e424a5b9c251b7e
Andrija <globalcro@gmail.com>
2022-04-27 23:40:06 +0200

    Removed the env command

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ return [
     'post_types' => array_map(function ($postType) {
         return esc_html($postType);
     }, unserialize(get_option('ctp_tables:tables_enabled')) ?: []),
-    'default_post_table' => env('DB_PREFIX', $wpdb->prefix) . 'posts',
-    'default_meta_table' => env('DB_PREFIX', $wpdb->prefix) . 'postmeta',
+    'default_post_table' => $wpdb->prefix . 'posts',
+    'default_meta_table' => $wpdb->prefix . 'postmeta',
 ];
